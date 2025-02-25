e05507776fa308184acfdb8657c8e3213a1d535f
JBRS307 <kuba.rekas.307@gmail.com>
2025-02-25 22:02:57 +0100

    SC-45 feature: Added functions to sync contacts and list contacts in CLI, untested

---
+++ src/cli.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use presage::model::contacts::Contact;
+use crate::contacts::list_contacts;
+
+fn print_contact(contact: &Contact) {
+    println!("Name: {}", contact.name);
+    if let Some(phone_number) = &contact.phone_number {
+        println!("Phone number: {}", phone_number);
+    }
+}
+
+pub async fn print_contacts() -> Result<()> {
+    let contacts = list_contacts().await?;
+    
+    for contact_res in contacts {
+        if let Ok(contact) = contact_res {
+            print_contact(&contact);
+            println!("================");
+        }
+    }
+    Ok(())
+}
---
+++ src/contacts.rs
@@ -0,0 +1,35 @@
+use anyhow::Result;
+use presage::Manager;
+use presage::model::{identity::OnNewIdentity, contacts::Contact};
+use presage::store::ContentsStore;
+use presage_store_sled::{MigrationConflictStrategy, SledStore, SledStoreError};
+
+use crate::paths;
+
+/// Function to sync contacts
+pub async fn sync_contacts() -> Result<()> {
+    let store = SledStore::open(
+        paths::STORE,
+        MigrationConflictStrategy::Drop,
+        OnNewIdentity::Trust,
+    )
+    .await?;
+    let mut manager = Manager::load_registered(store).await?;
+    manager.request_contacts().await?;
+    Ok(())
+}
+
+/// Returns iterator over stored contacts
+pub async fn list_contacts() -> Result<Vec<Result<Contact, SledStoreError>>> {
+    let store = SledStore::open(
+        paths::STORE,
+        MigrationConflictStrategy::Drop,
+        OnNewIdentity::Trust,
+    )
+    .await?;
+
+    
+    let manager = Manager::load_registered(store).await?;
+    let contacts: Vec<Result<Contact, SledStoreError>> = manager.store().contacts().await?.collect();
+    Ok(contacts)
+}
