713c67d2a45da3959965152c18ed91b78bc375ae
Kuba Rękas <74935888+JBRS307@users.noreply.github.com>
2025-04-07 09:31:41 +0200

    SC-67: extract manager creation (#10)
    * SC-67: added function to create registered manager
    
    * SC-67: Replaced manual manager creation with create_registered_manager()
    
    * SC-67: linter and formater changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use anyhow::{Error, Result};
+use presage::{
+    manager::{Manager, Registered},
+    model::identity::OnNewIdentity,
+};
+use presage_store_sled::{MigrationConflictStrategy, SledStore};
+
 pub mod app;
 pub mod args;
 pub mod cli;
@@ -9,3 +16,20 @@ pub mod tui;
 pub mod ui;
 
 pub mod sending {}
+
+/// Creates new manager in registered state
+pub async fn create_registered_manager() -> Result<Manager<SledStore, Registered>> {
+    let store = SledStore::open(
+        paths::STORE,
+        MigrationConflictStrategy::Drop,
+        OnNewIdentity::Trust,
+    )
+    .await?;
+
+    // Sadly it has to be done this way, because anyhow::Error doesn't cover errors
+    // from presage
+    match Manager::load_registered(store).await {
+        Ok(manager) => Ok(manager),
+        Err(err) => Err(Error::new(err)),
+    }
+}
