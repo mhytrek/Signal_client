134cfd018b092262a43cb452d124a96ffaa7600a
Jakub Rękas <kuba.rekas.307@gmail.com>
2025-08-21 21:25:16 +0200

    Created separate sending modules for groups and DMs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use anyhow::{Error, Result};
 use presage::{
-    libsignal_service::prelude::Uuid,
+    libsignal_service::{prelude::Uuid, zkgroup::GroupMasterKeyBytes},
     manager::{Manager, Registered},
-    model::{contacts::Contact, identity::OnNewIdentity},
+    model::{contacts::Contact, groups::Group, identity::OnNewIdentity},
 };
 use presage_store_sqlite::{SqliteConnectOptions, SqliteStore, SqliteStoreError};
 use std::sync::Arc;
@@ -29,6 +29,7 @@ pub mod sending {}
 pub type AsyncRegisteredManager = Arc<RwLock<Manager<SqliteStore, Registered>>>;
 
 pub type AsyncContactsMap = Arc<Mutex<HashMap<Uuid, Contact>>>;
+pub type AsyncGroupMap = Arc<Mutex<HashMap<GroupMasterKeyBytes, Group>>>;
 
 pub async fn open_store(path: &str) -> Result<SqliteStore, SqliteStoreError> {
     let options = SqliteConnectOptions::from_str(path)?.create_if_missing(true);
