ebcbbc06ef5de6944b3e7aaead60f3a8afe4940a
Jakub Rękas <kuba.rekas.307@gmail.com>
2025-12-25 21:13:08 +0100

    Disable logging by default for release profile

--- src/logger.rs
+++ src/logger.rs
@@ -10,7 +10,7 @@ use tracing::level_filters::LevelFilter;
 use tracing_appender::rolling;
 use tracing_subscriber::{EnvFilter, fmt::writer::BoxMakeWriter};
 
-use crate::env::{SIGNAL_LOGGER_LEVEL, SIGNAL_ONSCREEN_LOGGER};
+use crate::env::{SIGNAL_ENABLE_LOGGER, SIGNAL_LOGGER_LEVEL, SIGNAL_ONSCREEN_LOGGER};
 
 #[cfg(target_os = "macos")]
 fn ensure_log_dir() -> Result<PathBuf> {
@@ -53,29 +53,31 @@ fn logs_directory() -> PathBuf {
 }
 
 pub fn init_logger() {
-    let filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::INFO.into())
-        .with_env_var(SIGNAL_LOGGER_LEVEL)
-        .from_env()
-        .unwrap_or_default();
-    let onscreen_logs = env::var(SIGNAL_ONSCREEN_LOGGER).is_ok();
+    if cfg!(debug_assertions) || env::var(SIGNAL_ENABLE_LOGGER).is_ok() {
+        let filter = EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .with_env_var(SIGNAL_LOGGER_LEVEL)
+            .from_env()
+            .unwrap_or_default();
+        let onscreen_logs = env::var(SIGNAL_ONSCREEN_LOGGER).is_ok();
 
-    if onscreen_logs {
-        tracing_subscriber::fmt()
-            .with_env_filter(filter)
-            .with_ansi(true)
-            .init();
-    } else {
-        let file_writer = rolling::hourly(logs_directory(), "signal_client.log");
-        let (non_blocking, _guard) = tracing_appender::non_blocking(file_writer);
+        if onscreen_logs {
+            tracing_subscriber::fmt()
+                .with_env_filter(filter)
+                .with_ansi(true)
+                .init();
+        } else {
+            let file_writer = rolling::hourly(logs_directory(), "signal_client.log");
+            let (non_blocking, _guard) = tracing_appender::non_blocking(file_writer);
 
-        tracing_subscriber::fmt()
-            .with_writer(BoxMakeWriter::new(non_blocking))
-            .with_env_filter(filter)
-            .with_ansi(false)
-            .init();
+            tracing_subscriber::fmt()
+                .with_writer(BoxMakeWriter::new(non_blocking))
+                .with_env_filter(filter)
+                .with_ansi(false)
+                .init();
 
-        // Drops ownership of the value, but keeps it alive
-        std::mem::forget(_guard);
+            // Drops ownership of the value, but keeps it alive
+            std::mem::forget(_guard);
+        }
     }
 }
